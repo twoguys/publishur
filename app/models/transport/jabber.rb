c29287800e2ad379125554df5710c42df39465e6
Marshall Huss <mwhuss@gmail.com>
2010-01-17 10:34:01 -0500

    Jabber works

--- app/models/transport/jabber.rb
+++ app/models/transport/jabber.rb
@@ -1,7 +1,9 @@
 class Transport::Jabber < Transport::Base
   
   def perform
-    im = Jabber::Simple.new(ENV['JABBER_LOGIN'], ENV['JABBER_PASSWORD'])
+    login       = ENV['JABBER_LOGIN']     || 'publishur.messenger@gmail.com'
+    password    = ENV['JABBER_PASSWORD']  || 'b33r1sc00l'
+    im = Jabber::Simple.new(login, password)
     im.deliver(self.receiver, self.message)
   end
   
