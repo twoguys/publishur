bd6f4c5b287aadc7b7f18bfccbddb0ac828f6c42
Marshall Huss <mwhuss@gmail.com>
2010-01-17 11:34:35 -0500

    Jabber formatting

--- app/models/transport/jabber.rb
+++ app/models/transport/jabber.rb
@@ -3,7 +3,7 @@ class Transport::Jabber < Transport::Base
   def perform
     login       = ENV['JABBER_LOGIN']     || 'publishur.messenger@gmail.com'
     password    = ENV['JABBER_PASSWORD']  || 'b33r1sc00l'
-    im = Jabber::Simple.new(login, password)
+    im          = Jabber::Simple.new(login, password)
     im.deliver(self.receiver, self.message)
   end
   
