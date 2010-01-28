287335b7319ce6fb301d0817fc3466d934deef0b
David Browning <djbrowning@gmail.com>
2010-01-27 23:08:20 -0500

    quick bug fix in email notificaiton

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,7 +1,7 @@
 class Email < Subscription
   
   def perform
-    Notifications.deliver_message(self.contact_info, self.group, self.message.body)
+    Notifications.deliver_message(self.contact_info, self.group, self.message)
   end
   
   def print_description
