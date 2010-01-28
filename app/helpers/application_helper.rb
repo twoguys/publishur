44038da54b9118a60806ad2bbc186c5081397e61
Marshall Huss <mwhuss@gmail.com>
2010-01-27 22:23:24 -0500

    admin to admin?

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,7 +17,7 @@ module ApplicationHelper
     return "#{sub.contact_info}"                  if sub.is_a?(Tweet)
   end
   
-  def admin
+  def admin?
     current_user && current_user.admin?
   end
 end
