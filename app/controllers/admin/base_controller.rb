fb727ec4dbf524b67d61d80d89468535110df6c7
Marshall Huss <mwhuss@gmail.com>
2010-01-28 19:02:21 -0500

    Added gchartrb

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -3,6 +3,16 @@ class Admin::BaseController < ApplicationController
   before_filter { |c| c.nav :admin }
   
   def index
+    @subscription_chart = GoogleChart::PieChart.new('320x200', "Subscription Types", false) do |pc|
+      pc.data "AIM", AIM.count
+      pc.data "Jabber", JabberMessage.count
+      pc.data "Twitter", Tweet.count
+      pc.data "Email", Email.count
+      pc.data "HTTP Post", PostHook.count
+
+      # Pie Chart with no labels
+      pc.show_labels = true
+    end
   end
   
 end
