0810a37e434901d2b8d537e85815c5a1ffea3bd3
Marshall Huss <mwhuss@gmail.com>
2010-01-30 22:35:37 -0500

    Added staging environment

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,11 +1,11 @@
 class Admin::DashboardController < Admin::BaseController
   def index
-    @subscription_chart = GoogleChart::PieChart.new('320x200', "Subscription Types", false) do |pc|
-      pc.data "AIM", AIM.count
-      pc.data "Jabber", JabberMessage.count
-      pc.data "Twitter", Tweet.count
-      pc.data "Email", Email.count
-      pc.data "HTTP Post", PostHook.count
+    @subscription_chart = GoogleChart::PieChart.new('640x400', "Subscription Types", false) do |pc|
+      pc.data "AIM (#{AIM.count})", AIM.count
+      pc.data "Jabber (#{JabberMessage.count})", JabberMessage.count
+      pc.data "Twitter (#{Tweet.count})", Tweet.count
+      pc.data "Email (#{Email.count})", Email.count
+      pc.data "HTTP Post (#{PostHook.count})", PostHook.count
 
       # Pie Chart with no labels
       pc.show_labels = true
