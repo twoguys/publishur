9ac95025df74c3faf1f4dc0c772cf0bdf22e135a
Marshall Huss <mwhuss@gmail.com>
2010-02-20 15:27:20 -0500

    oops

--- app/controllers/group_memberships_controller.rb
+++ app/controllers/group_memberships_controller.rb
@@ -19,7 +19,7 @@ class GroupMembershipsController < ApplicationController
   
   def accept
     @group_membership.update_attribute(:state, "member")
-    Notifications.deliver_membership_accepted_notification(self)
+    Notifications.deliver_membership_accepted_notification(@group_membership)
     redirect_to @group
   end
   
